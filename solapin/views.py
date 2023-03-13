8fd101afd61e773d6d4dedf28732c7cbcaf229c3
lotamendi <lotamendi1991@gmail.com>
2023-03-13 09:42:54 -0700

    ksdhfkwdjhfkjsdhfks

--- solapin/views.py
+++ solapin/views.py
@@ -1,3 +1,5 @@
+import os
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,6 +8,8 @@ from solapin.forms import SolapinForm
 from solapin.mixins import ValidatePermissionRequiredMixin
 from solapin.models import SolapinPersona
 from config.settings import STATICFILES_DIRS
+from django.contrib import messages
+
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -83,76 +87,85 @@ class SolapinGenerateView(TemplateView):
     template_name = "generate.html"
     image = Image.open('static/UG/img/solapin/diseno1_limpio.png')
     diseno_superior = False
+    success_url = reverse_lazy('solapin_list')
+
     
     def dispatch(self, request, *args, **kwargs):
-        # quiero obtener el id de la persona para obtener todos sus datos
+        data = SolapinPersona.objects.get(pk=super().get_context_data(**kwargs)['pk'])
         datos = {
-            "no":3069,
-            "nombre":"IDORKYS IRSULA MARTINEZ",
-            "cargo":"SECRETARIA EJECUTIVA",
-            "area_general":"Facultad de Ingenieria \ny Ciencias Tecnicas",
-            "area_trabajo":"RECTORIA",
-            "foto":'',
-            'libre_acceso' : False,
-            'diseno_superior': False
+            "no":data.no,
+            "nombre":data.nombre,
+            "cargo":data.cargo,
+            "area_general":data.area_general,
+            "area_trabajo":data.area_trabajo,
+            "foto":data.foto,
+            'libre_acceso' : data.libre_acceso,
+            'diseno_superior': data.diseno_superior
             }
+        if datos['foto'] == '' or os.path.isfile(str(datos['foto'])):
+            messages.add_message(request, messages.ERROR, '{} {}'.format('No se ha encontrado la foto en la ruta especificada.', datos['foto']))
+            return redirect(self.success_url)
+        self.generar_solapin(datos, request)
+        return super().dispatch(request, *args, **kwargs)
+        
+    def generar_solapin(self, datos, request):
         self.diseno_superior = datos['diseno_superior']
         if datos['diseno_superior'] == True:
             self.image = Image.open('static/UG/img/solapin/diseno2_limpio.png').convert('RGBA')
         else:
             self.image = Image.open('static/UG/img/solapin/diseno1_limpio.png').convert('RGBA')
         self.texto_area_general(datos['area_general'])
-        self.texto_nombre(datos['nombre'])
-        self.texto_cargo(datos['cargo'])
-        self.texto_area_trabajo(datos['area_trabajo'])
-        self.imagen_foto()
+        self.texto_normal(datos['nombre'], (36, 430))
+        self.texto_normal(datos['cargo'], (36, 490))
+        self.texto_normal(datos['area_trabajo'], (36, 555))
+        self.imagen_foto(datos['foto'])
         if datos['libre_acceso']:
             self.texto_libre_acceso()
-        self.image.show()
+        titulo = '{}-{}.jpg'.format(datos['no'], datos['nombre']) 
+        self.image.show(titulo)
         # image.save("solapin/generate/test.jpg")
-        return super().dispatch(request, *args, **kwargs)
-        
-    def texto_area_general(self, texto):
+        messages.add_message(request, messages.SUCCESS, '{} {}'.format('Solapín generado satisfactoriamente.', datos['foto']))
 
+
+    def texto_area_general(self, texto):
         draw = ImageDraw.Draw(self.image)
         if self.diseno_superior:
             font = ImageFont.truetype("static/UG/font/AGaramondPro-Bold.otf", 25)
-            draw.multiline_text((33, 128), self.area_trabajo_cortada(str.upper(texto)) , font=font, fill="red")
+            draw.multiline_text((33, 125), self.cortar_texto(str.upper(texto), 30) , font=font, fill="red")
         else:
             font = ImageFont.truetype("static/UG/font/AGaramondPro-Regular.otf", 25)
-            draw.multiline_text((33, 128), self.area_trabajo_cortada(texto) , font=font, fill="white")
+            draw.multiline_text((33, 125), self.cortar_texto(texto, 30) , font=font, fill="white")
     
-    def texto_nombre(self, texto):
+    def texto_normal(self, texto, coordenadas):
         draw = ImageDraw.Draw(self.image)
         font = ImageFont.truetype("static/UG/font/Aller_Rg.ttf", 20)
-        draw.multiline_text((36, 440), self.area_trabajo_cortada(str.title(texto)) , font=font, fill="black")
-    
-    def texto_cargo(self, texto):
-        draw = ImageDraw.Draw(self.image)
-        font = ImageFont.truetype("static/UG/font/Aller_Rg.ttf", 20)
-        draw.multiline_text((36, 500), self.area_trabajo_cortada(str.title(texto)) , font=font, fill="black")
-    
-    def texto_area_trabajo(self, texto):
-        draw = ImageDraw.Draw(self.image)
-        font = ImageFont.truetype("static/UG/font/Aller_Rg.ttf", 20)
-        draw.multiline_text((36, 565), self.area_trabajo_cortada(str.title(texto)) , font=font, fill="black")
+        draw.text(coordenadas, self.cortar_texto(str.title(texto),40) , font=font, fill="black")
     
     def texto_libre_acceso(self):
         draw = ImageDraw.Draw(self.image)
         font = ImageFont.truetype("static/UG/font/Aller_Rg.ttf", 40)
-        draw.multiline_text((250, 200), self.area_trabajo_cortada('LIBRE \nACCESO') , font=font, fill="black", align="center")
+        draw.multiline_text((250, 200), self.cortar_texto('LIBRE \nACCESO', 20) , font=font, fill="black", align="center")
 
-    def imagen_foto(self):
+    def imagen_foto(self, ruta: str):
         im1 = self.image
-        im2 = Image.open('media/solapin/3254/IMG_2242.JPG').resize((174,186))
+        im2 = Image.open(ruta).resize((174,186))
         mask_im = Image.new("1", im2.size, 0)
         draw = ImageDraw.Draw(mask_im)
         draw.rectangle((0, 0, 354, 354), fill=255)
         im1.paste(im2,(22,203), mask_im)
 
-    def area_trabajo_cortada(self, nombre):
-        # IMPLEMENTAR
-        # limita la cantidad de caracteres e inserta un salto de linea
-        # temp = nombre[:30]
-        # "Facultad de Ingenieria \ny Ciencias Tecnicas"
-        return nombre
+    def cortar_texto(self, texto: str, max_caracteres: int):
+        salida = ''
+        arreglo_palabras = texto.split(' ')
+        linea = ''
+        for palabra in arreglo_palabras:
+            if len(linea) == 0:
+                linea += palabra + ' '
+            else:
+                if len(linea)+len(palabra) < max_caracteres:
+                    linea += palabra + ' '
+                else:
+                    salida += linea + '\n'
+                    linea = palabra + ' '
+        salida += linea
+        return salida
